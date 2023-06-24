e9af463b6d0ca870540f33ac2c8c4fef6eb02927
crefter <crefter1@yandex.ru>
2023-06-24 21:55:43 +0700

    update

--- make_a_choice/lib/widgets/left_box.dart
+++ make_a_choice/lib/widgets/left_box.dart
@@ -1,37 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:make_a_choice/main.dart';
 
-class LeftBoxAnimation extends StatefulWidget {
-  const LeftBoxAnimation({super.key});
-
-  @override
-  State<LeftBoxAnimation> createState() => _LeftBoxAnimationState();
-}
-
-class _LeftBoxAnimationState extends State<LeftBoxAnimation> {
-  bool value = true;
+class LeftBoxAnimation extends StatelessWidget {
+  final VoidCallback onClicked;
+  final bool isOpened;
+  LeftBoxAnimation({required this.onClicked, required this.isOpened, super.key});
 
   @override
   Widget build(BuildContext context) {
     return AnimatedCrossFade(
       duration: const Duration(seconds: 1),
       crossFadeState:
-          value ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+      isOpened ? CrossFadeState.showSecond : CrossFadeState.showFirst,
       firstChild: IconButton(
           icon: Image.asset('assets/images/b-box.png'),
           iconSize: 80,
           onPressed: () {
-            setState(() {
-              value = !value;
-
-              // startPosition = false;
-            });
+            onClicked();
           }),
       secondChild: IconButton(
           icon: Image.asset('assets/images/b-box-o.png'),
           alignment: Alignment.centerLeft,
           iconSize: 150,
-          onPressed: () {}),
+          onPressed: () {
+            onClicked();
+          }),
     );
   }
 }
