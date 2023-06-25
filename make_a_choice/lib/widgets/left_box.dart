43756c7dc4e2cac0fd795841d36a490954edf9ba
crefter <crefter1@yandex.ru>
2023-06-25 15:37:50 +0700

    animation

--- make_a_choice/lib/widgets/left_box.dart
+++ make_a_choice/lib/widgets/left_box.dart
@@ -7,23 +7,25 @@ class LeftBoxAnimation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedCrossFade(
-      duration: const Duration(seconds: 1),
-      crossFadeState:
-      isOpened ? CrossFadeState.showSecond : CrossFadeState.showFirst,
-      firstChild: IconButton(
-          icon: Image.asset('assets/images/b-box.png'),
-          iconSize: 80,
-          onPressed: () {
-            onClicked();
-          }),
-      secondChild: IconButton(
-          icon: Image.asset('assets/images/b-box-o.png'),
-          alignment: Alignment.centerLeft,
-          iconSize: 150,
-          onPressed: () {
-            onClicked();
-          }),
+    return Center(
+      child: AnimatedCrossFade(
+        duration: const Duration(seconds: 1),
+        crossFadeState:
+        isOpened ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+        firstChild: IconButton(
+            icon: Image.asset('assets/images/b-box.png'),
+            iconSize: 80,
+            onPressed: () {
+              onClicked();
+            }),
+        secondChild: IconButton(
+            icon: Image.asset('assets/images/b-box-o.png'),
+            alignment: Alignment.centerLeft,
+            iconSize: 150,
+            onPressed: () {
+              onClicked();
+            }),
+      ),
     );
   }
 }
